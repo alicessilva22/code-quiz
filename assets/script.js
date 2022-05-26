51dde2755282d446e87ad021eda92c80a0f78b63
Alice Silva <alicessilva@aol.com>
2022-05-26 16:49:12 -0400

    remove css

--- assets/script.js
+++ assets/script.js
@@ -1,41 +1,41 @@
-var state = 'quiz';
+// var state = 'quiz';
 
-var startEl = document.querySelector("#start");
-var quizEl = document.querySelector("#quiz");
-var endEl = document.querySelector("#end");
-var startBtn = document.querySelector("#start button");
-var quizTitle = document.querySelector("#quiz #title");
+// var startEl = document.querySelector("#start");
+// var quizEl = document.querySelector("#quiz");
+// var endEl = document.querySelector("#end");
+// var startBtn = document.querySelector("#start button");
+// var quizTitle = document.querySelector("#quiz #title");
 
-function displayState() {
-    if (state === 'start') {
-        startEl.style.display = 'block';
-        quizEl.style.display = 'none';
-        endEl.style.display = 'none';
-    }
-    if (state === 'quiz') {
-        startEl.style.display = 'none';
-        quizEl.style.display = 'block';
-        endEl.style.display = 'none';
-    }
-    if (state === 'end') {
-        startEl.style.display = 'none';
-        quizEl.style.display = 'none';
-        endEl.style.display = 'block';
-    }
-}
+// function displayState() {
+//     if (state === 'start') {
+//         startEl.style.display = 'block';
+//         quizEl.style.display = 'none';
+//         endEl.style.display = 'none';
+//     }
+//     if (state === 'quiz') {
+//         startEl.style.display = 'none';
+//         quizEl.style.display = 'block';
+//         endEl.style.display = 'none';
+//     }
+//     if (state === 'end') {
+//         startEl.style.display = 'none';
+//         quizEl.style.display = 'none';
+//         endEl.style.display = 'block';
+//     }
+// }
 
-function init() {
-    displayState();
-}
+// function init() {
+//     displayState();
+// }
 
-startBtn.addEventListener("click", function () {
-    state = 'quiz';
-    displayState();
-});
+// startBtn.addEventListener("click", function () {
+//     state = 'quiz';
+//     displayState();
+// });
 
-quizTitle.addEventListener("click", function () {
-    state = 'end';
-    displayState();
-});
+// quizTitle.addEventListener("click", function () {
+//     state = 'end';
+//     displayState();
+// });
 
-init();
+// init();
